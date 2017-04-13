884f7d59935bfffeb5b14b1f99f520e4da6737a6
alexdarling <aldarling@hotmail.com>
2017-04-12 22:01:40 -0400

    Adding preliminary code
    IT'S HAPPENING

--- bin/connect.py
+++ bin/connect.py
@@ -1,3 +1,23 @@
-import googleapiclient
 import openpyxl
 import requests
+
+
+def get_urls_from_excel_list(file_path):
+    urls = []
+    wb = openpyxl.load_workbook(file_path)
+    ws = wb.active
+    row_indices = [1 + x for x in range(0, ws.max_row)]
+    for row_index in row_indices:
+        url = ws['A' + str(row_index)].value
+        urls.append(url)
+    return urls
+
+
+def get_page_content(urls):
+    page_content = []
+    for url in urls:
+        r = requests.get(url)
+        r.encoding = 'utf-8'  # all CK sites should use this.
+        single_page_content = r.text
+        page_content.append(single_page_content)
+    return page_content
--- bin/scrape.py
+++ bin/scrape.py
@@ -1,5 +1,10 @@
 import requests
 import openpyxl
-# import organize
+from bs4 import BeautifulSoup
+# import organize?
+
+# Some syntax like this?
+#   str(soup.find_all('h1')[0].contents[0])
+
 
 test = "abc"
